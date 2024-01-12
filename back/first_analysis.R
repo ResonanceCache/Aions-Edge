41fb41c95b9b0e7c37508fc76890196c4b640368
Daniel Williamson <daniel@daniels-mbp.myfiosgateway.com>
2024-01-12 15:51:34 -0500

    add tao stuff

--- back/first_analysis.R
+++ back/first_analysis.R
@@ -24,7 +24,7 @@ tail(SPY)
 #Graph it
 chart_Series(SPY)
 zoom_Chart("2020")
-zoom_Chart("2023-01-01::2023-07-01")
+zoom_Chart("2023-06-01::2024-01-12")
 
 #Create Simple Moving Average
 sma <- SMA(x = Cl(SPY), n = 200)
@@ -37,3 +37,18 @@ add_TA(sma, on=1, lwd=1.5, col="blue")
 rsi <- RSI(price = Cl(SPY), n=2)
 class(rsi)
 
+# Create Exponentail Moving Average
+ema_8 <- EMA(x = Cl(SPY), n = 8)
+ema_21 <- EMA(x = Cl(SPY), n = 21)
+ema_34 <- EMA(x = Cl(SPY), n = 34)
+ema_55 <- EMA(x = Cl(SPY), n = 55)
+ema_89 <- EMA(x = Cl(SPY), n = 89)
+
+chart_Series(ema_21)
+
+add_EMA(n=8, col="red")
+add_EMA(n=21, col="blue")
+add_EMA(n=34, col="green")
+add_EMA(n=55, col="orange")
+add_EMA(n=89, col="purple")
+addADX(n = 14, maType="EMA", wilder=TRUE)
