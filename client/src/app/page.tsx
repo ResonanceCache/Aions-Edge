d692b4b26239e15afc4ef9746355d1d119a04b03
ResonanceCache <danielrwilliamson27@gmail.com>
2024-07-27 22:11:29 -0400

    add back in basic TRPC

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
-import { HydrateClient } from "~/trpc/server";
+import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
+  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   // void api.post.getLatest.prefetch();
+
+
 
   return (
     <HydrateClient>
 
       <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
         Aion&apos;s Edge
-      </h1>
 
+      </h1>
+      <div>
+        {hello ? hello.greeting : "Some Text"}
+      </div>
     </HydrateClient>
   );
 }
